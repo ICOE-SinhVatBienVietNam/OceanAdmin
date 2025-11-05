8a61fca0352bfd096e7fbbc4f488faf6ef854c0b
duy08k4 <duytran.290804@gmail.com>
2025-11-05 20:47:38 +0700

    Set up code base

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Router dom
+import { Routes } from "react-router-dom";
+
+const App: React.FC = () => {
+  return (
+    <div className="h-full w-full">
+      <Routes
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config/routeConfig.tsx
@@ -0,0 +1,36 @@
+export const routeConfig = {
+    intro: {
+        root: "/"
+    },
+    main: {
+        root: "/main",
+        map: "/main/map",
+        discover: "/main/discover",
+        camera: "/main/camera",
+        quest: "/main/quest",
+        moreInfo: "/main/more-info",
+    },
+    mainSlug: {
+        map: {
+
+        },
+        discover: {
+
+        },
+        camera: {
+
+        },
+        quest: {
+
+        },
+    },
+    login: {
+        root: "/login"
+    },
+    register: {
+        root: "/register"
+    },
+    share: {
+        species: "/public-shared/:id"
+    }
+}
