e02008ec02b043fe070b4891b3189ac519d3675b
OCEANVN <sinhvatbienvietnam.app@gmail.com>
2026-01-07 13:38:25 +0700

    Dev (#3)
    * UI(refactor): Complete implementing the Login page for admin
    
    * 'ui'
    
    * ui/addMultiSpecies: Completed but it need re-style ui
    
    * feat: Add one species and delete species feature
    
    * refactor: adjust SpeciesDetailModal
    
    * chore: change the app's name
    
    * chore: remove declared variable
    
    * fix: vercel 404
    
    ---------
    
    Co-authored-by: duy08k4 <duytran.290804@gmail.com>

--- src/ui/layout/mainLayout.tsx
+++ src/ui/layout/mainLayout.tsx
@@ -15,10 +15,14 @@ import { Auth } from "../component/Auth";
 import UserPage from "../page/User";
 import QuestionPage from "../page/Question";
 import ContributionPage from "../page/Contribution";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
 
 const MainLayout: React.FC = () => {
   // Location
   const pathLocation = useLocation();
+
+  const authData = useSelector((state: RootState) => state.adminReducer.user)
 
   // Menu
   const menu = useRef<{ content: string; icon: ReactNode; path: string }[]>([
@@ -144,12 +148,12 @@ const MainLayout: React.FC = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                className="size-5"
+                className="size-6"
               >
                 <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
                 <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
               </svg>
-              duytran.290804@gmail.com
+              {authData?.email}
             </p>
           </span>
         </div>
